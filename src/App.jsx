1fbad44f759ac875b0249022fdac768f8fbba592
omjaiswal45 <omjai11022000@gmail.com>
2025-05-17 02:37:24 +0530

    additems

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,13 @@ function App() {
     { todoName: "buy milk", todoDate: "01/10/2025" },
     { todoName: "buy banana", todoDate: "02/10/2025" },
     { todoName: "buy eggs", todoDate: "03/10/2025" },
+    { todoName: "buy bread", todoDate: "04/10/2025" },
+    { todoName: "buy butter", todoDate: "05/10/2025" },
+    { todoName: "buy sugar", todoDate: "06/10/2025" },
+    { todoName: "buy salt", todoDate: "07/10/2025" },
+    { todoName: "buy rice", todoDate: "08/10/2025" },
+    { todoName: "buy flour", todoDate: "09/10/2025" },
+    { todoName: "buy oil", todoDate: "10/10/2025" },
   ];
   return (
     <center class="todo-container">
